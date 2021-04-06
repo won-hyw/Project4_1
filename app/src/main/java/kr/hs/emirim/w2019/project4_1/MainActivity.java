ecf9a526ee50e18f1c0c853456a209246e9b6202
choi ji won <w2019@e-mirim.hs.kr>
2021-04-06 20:08:57 +0900

    Layout & Java1 commit

---
+++ app/src/main/java/kr/hs/emirim/w2019/project4_1/MainActivity.java
@@ -0,0 +1,31 @@
+package kr.hs.emirim.w2019.project4_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText edit1, edit2;
+    TextView textResult;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnPlus = findViewById(R.id.btn1);
+        Button btnMinus = findViewById(R.id.btn2);
+        Button btnMulti = findViewById(R.id.btn3);
+        Button btnDiv = findViewById(R.id.btn4);
+        edit1 = findViewById(R.id.edit1);
+        edit2 = findViewById(R.id.edit2);
+        textResult = findViewById(R.id.t1);
+
+//        btnPlus.setOnTouchListener(btnListener);
+//        btnMinus.setOnTouchListener(btnListener);
+//        btnMulti.setOnTouchListener(btnListener);
+//        btnDiv.setOnTouchListener(btnListener);
+    }
+}
