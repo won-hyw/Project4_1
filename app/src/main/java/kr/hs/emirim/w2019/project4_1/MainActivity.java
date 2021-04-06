9b5486dbc32a9f4268e33eb29def6291679fc1d6
choi ji won <w2019@e-mirim.hs.kr>
2021-04-06 20:33:11 +0900

    complete Simple Calculator

--- app/src/main/java/kr/hs/emirim/w2019/project4_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2019/project4_1/MainActivity.java
@@ -2,6 +2,8 @@ package kr.hs.emirim.w2019.project4_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.EditText;
@@ -23,9 +25,37 @@ public class MainActivity extends AppCompatActivity {
         edit2 = findViewById(R.id.edit2);
         textResult = findViewById(R.id.t1);
 
-//        btnPlus.setOnTouchListener(btnListener);
-//        btnMinus.setOnTouchListener(btnListener);
-//        btnMulti.setOnTouchListener(btnListener);
-//        btnDiv.setOnTouchListener(btnListener);
+        btnPlus.setOnTouchListener(btnListener);
+        btnMinus.setOnTouchListener(btnListener);
+        btnMulti.setOnTouchListener(btnListener);
+        btnDiv.setOnTouchListener(btnListener);
     }
+
+    View.OnTouchListener btnListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            String strEdit1 = edit1.getText().toString();
+            String strEdit2 = edit2.getText().toString();
+            int num1 = Integer.parseInt(strEdit1);
+            int num2 = Integer.parseInt(strEdit2);
+            int result = 0;
+            switch (v.getId()){
+                case R.id.btn1:
+                    result = num1 + num2;
+                    break;
+                case R.id.btn2:
+                    result = num1 - num2;
+                    break;
+                case R.id.btn3:
+                    result = num1 * num2;
+                    break;
+                case R.id.btn4:
+                    result = num1 / num2;
+                    break;
+            }
+            textResult.setText(R.string.t1);
+            textResult.append(result + "");
+            return false;
+        }
+    };
 }
