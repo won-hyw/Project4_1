4f799bee8784211563493ea753fb513178420cf3
choi ji won <w2019@e-mirim.hs.kr>
2021-04-08 20:37:34 +0900

    Update Divide By Zero

--- app/src/main/java/kr/hs/emirim/w2019/project4_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2019/project4_1/MainActivity.java
@@ -59,9 +59,17 @@ public class MainActivity extends AppCompatActivity {
                     result = num1 * num2;
                     break;
                 case R.id.btn4:
+                    if(strEdit2.equals("0")) {
+                        Toast.makeText(getApplicationContext(), "0으로 나눗셈을 할 수 없어요.", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
                     result = num1 / num2;
                     break;
                 case R.id.btn5:
+                    if(strEdit2.equals("0")) {
+                        Toast.makeText(getApplicationContext(), "0으로 나눗셈을 할 수 없어요.", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
                     result = num1 % num2;
                     break;
             }
