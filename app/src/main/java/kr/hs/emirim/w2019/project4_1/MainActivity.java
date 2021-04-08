6ced84aca97ffe4912e1fa2acbee6110daab6f42
choi ji won <w2019@e-mirim.hs.kr>
2021-04-08 19:23:28 +0900

    Update Add Mod Button

--- app/src/main/java/kr/hs/emirim/w2019/project4_1/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2019/project4_1/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         Button btnMinus = findViewById(R.id.btn2);
         Button btnMulti = findViewById(R.id.btn3);
         Button btnDiv = findViewById(R.id.btn4);
+        Button btnMod = findViewById(R.id.btn5);
         edit1 = findViewById(R.id.edit1);
         edit2 = findViewById(R.id.edit2);
         textResult = findViewById(R.id.t1);
@@ -29,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         btnMinus.setOnClickListener(btnListener);
         btnMulti.setOnClickListener(btnListener);
         btnDiv.setOnClickListener(btnListener);
+        btnMod.setOnClickListener(btnListener);
     }
 
     View.OnClickListener btnListener = new View.OnClickListener() {
@@ -51,6 +53,9 @@ public class MainActivity extends AppCompatActivity {
                     break;
                 case R.id.btn4:
                     result = num1 / num2;
+                    break;
+                case R.id.btn5:
+                    result = num1 % num2;
                     break;
             }
             textResult.setText(R.string.t1);
